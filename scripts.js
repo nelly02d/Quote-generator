ab08b381ab874ed766e5052b89703e6539127d48
Arnel <nelly02d@gmail.com>
2023-03-04 10:50:43 -0500

    generate a promise that translate JSON to text

--- scripts.js
+++ scripts.js
@@ -2,7 +2,12 @@ const newQuote = () => {
   $.ajax({
     type: 'GET',
     url: 'https://type.fit/api/quotes', //fetching data from the api
-   
+    success: quotes => {
+      const parseData = JSON.parse(quotes); //translating success quotes data into object
+      const randomQuote = Math.floor(Math.random() * parseData.length); //get random index of quotes
+      
+      
+    }
   }); 
 };
 
